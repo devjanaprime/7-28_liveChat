1fed699d6548bb20dfdc280a42a91d83b3cf066b
devjanaprime <dev@primeacademy.io>
2016-07-28 18:00:09 -0500

    first commit!

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+console.log( 'HEEEEEEEYYYY!!!!' );
+
+var thisIsAFunction = function(){
+  alert( "Nate knows what to do");
+}
+
+function thisIsAnotherFunction(){
+  console.log( 'from thisIsAnotherFunction that Ben Mahowald thinks is "much better"!');
+
+  var theCoolFolks = [ 'Ben J', 'Ben M', 'John', 'Justin', 'Kevin', 'Matt', 'Nate' ];
+
+  console.log( "these are the cool folks:", theCoolFolks );
+  console.log( "How many cool folks are there?", theCoolFolks.length );
+}
+
+// copied from Nate
+thisIsAFunction();
+thisIsAnotherFunction();
