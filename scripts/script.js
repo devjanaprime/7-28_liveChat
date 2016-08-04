1c6ec12984dd58bb72f7924ceb362066f9f9dd19
Millie Walsh <millie@primeacademy.io>
2016-08-04 17:59:27 -0500

    added loop and greeting

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,22 @@ console.log( 'HEEEEEEEYYYY!!!!' );
 
 var thisIsAFunction = function(){
   alert( "Nate knows what to do");
-}
+};
 
-function thisIsAnotherFunction(){
+function thisIsAnotherFunction(greeting){
   console.log( 'from thisIsAnotherFunction that Ben Mahowald thinks is "much better"!');
 
-  var theCoolFolks = [ 'Ben J', 'Ben M', 'John', 'Justin', 'Kevin', 'Matt', 'Nate' ];
+  var theCoolFolks = [ 'Ben J', 'Ben M', 'John', 'Justin', 'Kevin', 'Matt', 'Nate', 'Millie', 'Dev' ];
 
   console.log( "these are the cool folks:", theCoolFolks );
   console.log( "How many cool folks are there?", theCoolFolks.length );
+
+  for (var i = 0; i < theCoolFolks.length; i++) {
+    console.log(greeting + ", " + theCoolFolks[i] );
+  }
+
 }
 
 // copied from Nate
 thisIsAFunction();
-thisIsAnotherFunction();
+thisIsAnotherFunction("hello");
