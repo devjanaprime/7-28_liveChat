7b02a01c792399e1d4262c5a3816476c09b0ae3c
devjanaprime <dev@primeacademy.io>
2016-08-11 18:01:34 -0500

    update for 8-11-2016

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,30 @@ var thisIsAFunction = function(){
 
 function thisIsAnotherFunction(greeting){
   console.log( 'from thisIsAnotherFunction that Ben Mahowald thinks is "much better"!');
+  // start off with an empty array
+  // into which we'll PUSH objects of cool folks
+  var theCoolFolks = [];
 
-  var theCoolFolks = [ 'Ben J', 'Ben M', 'John', 'Justin', 'Kevin', 'Matt', 'Nate', 'Millie', 'Dev' ];
+  var newPerson ={
+    'name': 'Millie',
+    'homeTown': "Shoreview... suburb of StP"
+  };
 
+  console.log( 'newPerson:', newPerson );
+  // push Millie into the Cool FOlks
+  theCoolFolks.push( newPerson );
+  // create dEv
+  newPerson ={
+    'name': 'dEv',
+    'homeTown': "dreary Erie, the mistake on the lake"
+  };
+  // force the cool kids to invite dEv to the birthday party
+  theCoolFolks.push( newPerson );
   console.log( "these are the cool folks:", theCoolFolks );
   console.log( "How many cool folks are there?", theCoolFolks.length );
 
   for (var i = 0; i < theCoolFolks.length; i++) {
-    console.log(greeting + ", " + theCoolFolks[i] );
+    console.log(greeting + ", " + theCoolFolks[i].name + " who is from " + theCoolFolks[i].homeTown );
   }
 
 }
